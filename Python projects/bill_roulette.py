267fba4a6bb224d7eed15787f7e5f26e6b582438
XHassan2003 <132691813+XHassan2003@users.noreply.github.com>
2023-06-17 21:08:17 +0500

    Delete Python projects directory

--- Python projects/bill_roulette.py
+++
@@ -1,7 +0,0 @@
-import random
-people = input("Enter your names serperated by comma: ")
-names = people.split(", ")
-num_items = len(names)
-random_choice = random.randint(0, num_items - 1)
-list1 = names[random_choice]
-print(list1, "is going to buy the meal today!")
--- Python projects/bmi_calculator.py
+++
@@ -1,7 +0,0 @@
-# BMI Calculator:
-
-weight = float(input("Enter you weight in kg: "))
-height = float(input("Enter your height in m: "))
-
-bmi = round(weight / height ** 2)
-print(bmi) 
